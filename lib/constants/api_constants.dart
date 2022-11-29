ca1709e5fe07673f24341290edfd8c0e35cd62db
Faisal Ansari <faisal.ansari@cupidknot.com>
2022-11-29 15:27:01 +0530

    server changed to UAT

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,11 +1,10 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConstants {
   static const localUrl = 'http://13.55.174.146:3000';
   static const liveUrl = 'https://app.itrakassets.com';
   // static const devBaseUrl = 'http://13.55.174.146:3001';
 
-  static const baseUrl = kDebugMode ? localUrl : liveUrl;
+  // static const baseUrl = kDebugMode ? localUrl : liveUrl;
+  static const baseUrl = localUrl;
   //
 
   static const mapsKey = 'AIzaSyBxMIupdGzYQM6yk1ix1xGhgIyPw_42wlI';
