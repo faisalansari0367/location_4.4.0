010eff1d062e32c45f5f7d20e274e43195d4e249
Faisal Ansari <faisal.ansari@cupidknot.com>
2023-01-04 11:12:09 +0530

    qr code fix

--- lib/ui/visitor_check_in/view/qr_code_widget.dart
+++ lib/ui/visitor_check_in/view/qr_code_widget.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:bioplus/constants/index.dart';
 import 'package:flutter/material.dart';
 
 class QRCodeImage extends StatefulWidget {
-  const QRCodeImage({super.key});
+  final String image;
+  const QRCodeImage({super.key, required this.image});
 
   @override
   State<QRCodeImage> createState() => _QRCodeImageState();
@@ -12,15 +15,15 @@ class _QRCodeImageState extends State<QRCodeImage> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 75.width,
-      height: 75.width,
+      width: 55.width,
+      height: 55.width,
       child: _qrCode(),
     );
   }
 
   Widget _qrCode() {
-    return Image.asset(
-      'assets/images/app_link_qr_code.png',
+    return Image.memory(
+      base64Decode(widget.image),
       fit: BoxFit.fitHeight,
       width: 75.width,
       height: 75.width,
